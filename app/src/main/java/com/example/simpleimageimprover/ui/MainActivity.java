0e28788cb5937d5eea48ec66369da48c7aee02a9
Pawan <iampawan17@gmail.com>
2021-05-31 17:53:30 +0530

    Image Picker Added

--- app/src/main/java/com/example/simpleimageimprover/ui/MainActivity.java
+++ app/src/main/java/com/example/simpleimageimprover/ui/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && null != data)  {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null)  {
             //TODO: action
             Intent intent = new Intent(this, ResultActivity.class);
             intent.putExtra(HelperClass.REAL_URI, data.getData().toString());
